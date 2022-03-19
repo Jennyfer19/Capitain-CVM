05ef96bf178b8e6fbabdb1e389330f01334071d9
Jennyfer19 <74720370+Jennyfer19@users.noreply.github.com>
2022-03-19 00:42:51 -0400

    add bat

---
+++ Assets/Scripts/Ennemy/BatBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatBehaviour : MonoBehaviour
+{
+  
+    [SerializeField]
+    public const float _delaisDestruction = 1f;
+    public float _debutTempsDestruction;
+    private bool _batMort = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_batMort)
+            if (Time.fixedTime > _debutTempsDestruction + _delaisDestruction) // laisse un peu de temps pour pourvoir sauter de la chauve-souris a la plateform
+            {
+                this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                this.gameObject.GetComponent<BatPatrol>().enabled = false;
+            }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            PlayerBehaviour pb = collision.gameObject.GetComponent<PlayerBehaviour>();
+            if (pb != null)
+                pb.CallEnnemyCollision();
+        }
+    }
+
+    // seulement un trigger sur le haut de la chauve-souris
+    // si player ecrase la tete de la chave-souris, alors ce dernier meurt
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            _debutTempsDestruction = Time.fixedTime; 
+            Debug.Log("hit");
+            _batMort = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Ennemy/BatPatrol.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class BatPatrol : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _vitesse = 3f;
+    [SerializeField]
+    private Transform[] _points;
+
+    private Transform _cible = null;
+    private SpriteRenderer _sr;
+    private float _distanceSeuil = 0.3f;
+    private int _indexPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sr = this.GetComponent<SpriteRenderer>();
+        _indexPoint = 0;
+        _cible = _points[_indexPoint];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = _cible.position - this.transform.position;
+        this.transform.Translate(direction.normalized * _vitesse * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(this.transform.position, _cible.position) < _distanceSeuil)
+        {
+            _indexPoint = (++_indexPoint) % _points.Length;
+            _cible = _points[_indexPoint];
+        }
+    }
+
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        // Ligne entre les cibles
+        for (int i = 0; i < _points.Length - 1; i++)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(_points[i].position,
+                _points[i + 1].position);
+        }
+
+        // Ligne entre l'ennemi et la cible
+        if (_cible != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(this.transform.position, _cible.position);
+        }
+    }
+
+#endif
+}
