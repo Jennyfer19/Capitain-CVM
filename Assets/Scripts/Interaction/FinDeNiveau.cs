db0941a88e1b6ade9727f9227a33ddd25f2f133f
Jennyfer19 <74720370+Jennyfer19@users.noreply.github.com>
2022-03-20 20:03:51 -0400

    add collectable panel

--- Assets/Scripts/Interaction/FinDeNiveau.cs
+++ Assets/Scripts/Interaction/FinDeNiveau.cs
@@ -1,15 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FinDeNiveau : MonoBehaviour
 {
+    [SerializeField]
+    private String next_scene;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Félicitation, le niveau est terminé.");
+            Debug.Log(SceneManager.GetActiveScene().name);
             GameManager.Instance.SaveData();
-            SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene(next_scene);
+
         }
     }
 }
+
